71ba3e00bcfc2c00d7d2cfd8d84139b0a437a55c
mattgaither <benjamin.gaither@shipt.com>
2022-07-04 17:48:22 -0500

    trying to get the bokks to show up in the liveserver

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,9 +1,37 @@
 //handlebars routes //open
-
 const router = require("express").Router();
-router.get("/" , (req,res)=> {
-    res.render("home")
-})
+const { Book } = require('../models');
+const withAuth = require('../utils/auth')
+
+
+router.get("/", async (req, res)=> {
+    try {
+        const dbBookData = await Book.findAll({
+            include: [
+                {
+                    // model: Book,
+                    attributes: [
+                        'id',
+                        'title',
+                        'author',
+                        'published_date',
+                        'file_name',
+                        'pages'
+                    ],
+                },
+            ],
+            raw: true,
+        });
+
+        const book = dbBookData.map({plain : true}) 
+
+        res.render( 'book', { book });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 router.get("/booklist", (req,res) =>{
     //book.findname.route //include books for author
 })
