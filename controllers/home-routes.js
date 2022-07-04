6c98482445539626a42971a66a070f95707c952e
alanam79 <halo2382001@gmail.com>
2022-07-04 15:06:54 -0400

    working on homepage set up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,8 +5,22 @@ router.get("/", (req, res) => {
   res.render("homepage");
 });
 
-router.get("/booklist", (req, res) => {
-  //book.findname.route //include books for author
+router.get("/", (req, res) => {
+  res.render("homepage", {
+    id: 1,
+    post_url: "https://handlebarsjs.com/guide/",
+    title: "Handlebars Docs",
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: "test_user",
+    },
+  });
 });
+
+// router.get("/booklist", (req, res) => {
+//book.findname.route //include books for author
+// });
 
 module.exports = router;
