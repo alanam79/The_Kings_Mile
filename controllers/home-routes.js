d289406dccc28637c529f831f26dfb6ff7e5fe28
alanam79 <halo2382001@gmail.com>
2022-07-06 14:04:31 -0400

    working on getting the second book handlebar page to render

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -57,6 +57,70 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// testing for below route
+// const book = {
+//   id: 1,
+//   book_url: "https://handlebarsjs.com/guide/",
+//   title: "Handlebars Docs",
+//   created_at: new Date(),
+//   vote_count: 10,
+//   comments: [{}, {}],
+//   user: {
+//     username: "test_user",
+//   },
+// };
+
+// show one book
+router.get("/book/:id", (req, res) => {
+  Book.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      // "book_url", left to build in if wanted to set link to amazon purchase
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE book.id = vote.book_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "book_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBookData) => {
+      if (!dbBookData) {
+        res.status(404).json({ message: "No book found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const book = dbBookData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-book", { book });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // router.get("/", async (req, res) => {
 //   try {
 //     const dbBookData = await Book.findAll({
