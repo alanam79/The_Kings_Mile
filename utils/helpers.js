a4abd730c22015daab61bf8acf55fcbd27023b37
alanam79 <halo2382001@gmail.com>
2022-07-08 19:50:36 -0400

    added helpers to correct plural words showing

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,25 @@
 module.exports = {
-    format_time: (date) => {
-        return date.toLocaleTimeString();
-    },
-    format_date: (date) => {
-        return `${new Date(date).getMonth() + 1}
+  format_time: (date) => {
+    return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}
         /${new Date(date).getDate()}
-        /${new Date(date).getFullYear() + 5}`;
-    },
+        /${new Date(date).getFullYear()}`;
+  },
+  format_url: (url) => {
+    return url
+      .replace("http://", "")
+      .replace("https://", "")
+      .replace("www.", "")
+      .split("/")[0]
+      .split("?")[0];
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  },
 };
